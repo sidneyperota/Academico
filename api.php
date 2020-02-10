d2c43c1066389f61539d68050e2338042c1bc382
sidneyperota <sidneyperota@gmail.com>
2020-02-10 17:42:14 -0300

    Implementacao do metodo Post com JSON

--- api.php
+++ api.php
@@ -49,6 +49,31 @@
         $url = explode('/', $_SERVER['PATH_INFO'] );
         array_shift($url);
         var_dump($url);
+        echo json_encode( $_REQUEST );
+        var_dump( $_POST );
+
+        //var_dump( $_SERVER ); 
+
+        // lê o json diretamente dos dados enviados no POST (input)
+        $json = file_get_contents('php://input');
+        $obj_php = json_decode($json); // $obj_php agora é exatamente o objeto/array enviado pelo servidor
+
+        print_r( $obj_php );
+
+
+        //header("Access-Control-Allow-Origin: *");
+        //header('Cache-Control: no-cache, must-revalidate'); 
+        //header("Content-Type: text/plain; charset=UTF-8");
+        //header("HTTP/1.1 200 OK");
+
+        $dados = file_get_contents("php://input");
+        echo $dados;
+
+
+            
+
+
+
      }
 
 
--- banco.php
+++ banco.php
@@ -7,20 +7,18 @@
 // Servidor Web  
 
  
+  /*
   $servidor ='localhost';
   $bdUsuario = 'u355362813_usuario';
   $bdSenha ='arrozal40';
   $bdBanco ='u355362813_financeiro';
- 
+  */ 
 
 
-
-  /*
   $bdServidor='localhost:3306';
   $bdUsuario= 'root';
   $bdSenha='si232317';
   $bdBanco='financeiro';
-  */
   
 
   $conexao = mysqli_connect( $bdServidor, $bdUsuario, $bdSenha, $bdBanco );
@@ -31,7 +29,7 @@
     echo '<h1>falha na conexao: </h1>'.$conexao-> connect_error;   
   } 
   
-
+  
   return $conexao; 
 
   }
