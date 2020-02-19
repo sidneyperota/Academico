09ae7f926969af8e87dd44a083e51c1fc8176488
sidneyperota <sidneyperota@gmail.com>
2020-02-19 17:11:56 -0300

    teste include

--- Teste2.php
+++ Teste2.php
@@ -1,39 +1,13 @@
 <?php 
 	
-	
-	$caminho = $_SERVER['HTTP_HOST']; 
-	echo $caminho; 
-	
-	//include $caminho."\\"."Global.php";
-	//include "Global.php";
+	include "dao/MovimentoCaixaDAO.php";   
+	include "controle/MovimentoCaixa.php";
+
+
+	echo "teste carregado com sucesso!";
+	  
 
 	
-	/*
-	include $caminho."\\dao\\MovimentoCaixaDAO.php";
-	include $caminho."\\controle\\MovimentoCaixa.php";
-
-
-	$movimentoCaixaDAO = new MovimentoCaixaDAO(); 
-
-	$movimentoCaixa = new MovimentoCaixa();  
-
-	$movimentoCaixa->getCaixa()->setData("2019-12-01");
-	$movimentoCaixa->setConta("12232");
-	$movimentoCaixa->setOperacao("E");
-	$movimentoCaixa->setHistorico("Recebido PAMPAS.");
-	$movimentoCaixa->setValor("155.00");
-	$movimentoCaixa->setUsuario("1"); 
-	$movimentoCaixa->setDoc("dsjdhs1212125");
-	$movimentoCaixaDAO->gravar($movimentoCaixa);
-
-	echo "Parabéns, gravado com Sucesso!";
-
-	echo "teste";
-	*/
-
-	//$caminho = $_SERVER['DOCUMENT_ROOT']; 
-	//echo $_SERVER['HTTP_HOST']; 
-	//$obj_var = new Variaveis(); 
 	
 	
 
